1606a5cfac3cb141e98e9e78d17bf120ba63b501
Nowtryz <damien.djmb@gmail.com>
2020-04-29 11:11:33 +0200

    add first database migration

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Quiz
+Quiz
+====
+
+## Installation
+
+1. Create a `.env.local` file at the root of the project
+1. Put the following into the file
+    ```env
+    DATABASE_URL="mysql://db_user:db_password@127.0.0.1:3306/db_name"
+    ```
+1. Install dependencies
+    ```shell script
+    php composer.phar install
+    ```
+1. Set up database
+    ```shell script
+    php bin/console doctrine:database:create # if database doesn't exist
+    php bin/console doctrine:migrations:migrate
+    ```
---
+++ src/Migrations/Version20200429090821.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200429090821 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, quizz_id INT NOT NULL, text LONGTEXT NOT NULL, INDEX IDX_B6F7494EBA934BCD (quizz_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE quizz (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reponse (id INT AUTO_INCREMENT NOT NULL, question_id INT DEFAULT NULL, text LONGTEXT NOT NULL, is_true TINYINT(1) DEFAULT NULL, INDEX IDX_5FB6DEC71E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(20) NOT NULL, email VARCHAR(50) NOT NULL, mdp VARCHAR(50) NOT NULL, score BIGINT DEFAULT NULL, is_admin TINYINT(1) DEFAULT NULL, description LONGTEXT DEFAULT NULL, image LONGTEXT DEFAULT NULL, UNIQUE INDEX UNIQ_1D1C63B3EEDA5EFE (mdp), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EBA934BCD FOREIGN KEY (quizz_id) REFERENCES quizz (id)');
+        $this->addSql('ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC71E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC71E27F6BF');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EBA934BCD');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE quizz');
+        $this->addSql('DROP TABLE reponse');
+        $this->addSql('DROP TABLE utilisateur');
+    }
+}
