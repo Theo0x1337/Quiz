9ebf50af39236fd58bbe9fe31b50aa9f54d099f7
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-05-04 15:01:39 +0200

    Update QuizController.php

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -9,8 +9,11 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Entity\Quizz;
+use App\Entity\Reponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class QuizController extends AbstractController
 {
@@ -36,21 +39,57 @@ class QuizController extends AbstractController
         ]);
     }
 
-    public function validerQuestion()
+    public function validerQuestion(Request $request)
     {
-        $question = $_POST['question'];
-        $bonneRep = $_POST['bonneRep'];
-        $mauvaiseRep1 = $_POST['mauvaiseRep1'];
+        $entityManager = $this->getDoctrine()->getManager();
 
-        if(isset($_POST['mauvaiseRep2'])){
-            $mauvaiseRep2 = $_POST['mauvaiseRep2'];
-        }
-        if(isset($_POST['mauvaiseRep3'])){
-            $mauvaiseRep3 = $_POST['mauvaiseRep3'];
+
+        $idQuiz = $request->query->get('idQuiz');
+        $question = $request->query->get('question');
+        var_dump($question);
+        $bonneRep = $request->query->get('bonneRep');
+        $mauvaiseRep1 = $request->query->get('mauvaiseRep1');
+        $mauvaiseRep2 = $request->query->get('mauvaiseRep2');
+        $mauvaiseRep3 = $request->query->get('mauvaiseRep3');
+
+
+        $objectQuestion = new Question();
+        $objectQuestion->setText($question);
+        $objectQuizz = $this->getDoctrine()->getRepository(Quizz::class)->find($idQuiz);
+        $objectQuestion->setQuizz($objectQuizz);
+        $entityManager->persist($objectQuestion);
+
+        $objectBonneReponse = new Reponse();
+        $objectBonneReponse->setText($bonneRep);
+        $objectBonneReponse->setQuestion($objectQuestion);
+        $objectBonneReponse->setIsTrue(True);
+        $entityManager->persist($objectBonneReponse);
+
+        $objectMauvaiseReponse1 = new Reponse();
+        $objectMauvaiseReponse1->setText($mauvaiseRep1);
+        $objectMauvaiseReponse1->setQuestion($objectQuestion);
+        $objectMauvaiseReponse1->setIsTrue(False);
+        $entityManager->persist($objectMauvaiseReponse1);
+
+        if (isset($mauvaiseRep2)){
+            $objectMauvaiseReponse2 = new Reponse();
+            $objectMauvaiseReponse2->setText($mauvaiseRep2);
+            $objectMauvaiseReponse2->setQuestion($objectQuestion);
+            $objectMauvaiseReponse2->setIsTrue(False);
+            $entityManager->persist($objectMauvaiseReponse2);
         }
 
-        
-        return $this->render('validerQuestion.html.twig');
+        if (isset($mauvaiseRep3)){
+            $objectMauvaiseReponse3 = new Reponse();
+            $objectMauvaiseReponse3->setText($mauvaiseRep3);
+            $objectMauvaiseReponse3->setQuestion($objectQuestion);
+            $objectMauvaiseReponse3->setIsTrue(False);
+            $entityManager->persist($objectMauvaiseReponse3);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('accueil');
     }
 
 
